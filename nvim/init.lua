efd5cc1caafc0a0030d433a72785a05cd02f4120
yonaisatoru <4207572+poindre@users.noreply.github.com>
2025-03-09 17:51:29 +0900

    copilot

--- nvim/init.lua
+++ nvim/init.lua
@@ -80,7 +80,6 @@ require('lazy').setup({
     config = function()
       vim.g.copilot_no_tab_map = true
       local keymap = vim.keymap.set
-      -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
       keymap(
         "i",
         "<C-g>",
@@ -92,6 +91,79 @@ require('lazy').setup({
       keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
       keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
     end,
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "gtihub/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    config = function ()
+      require("CopilotChat").setup({
+        show_help = "yes",
+        prompts = {
+          Explain = {
+            prompt = "/COPILOT_EXPLAIN コードを日本語で説明してください",
+            mapping = '<leader>ce',
+            description = "コードの説明をお願いする",
+          },
+          Review = {
+            prompt = '/COPILOT_REVIEW コードを日本語でレビューしてください。',
+            mapping = '<leader>cr',
+            description = "コードのレビューをお願いする",
+          },
+          Fix = {
+            prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
+            mapping = '<leader>cf',
+            description = "コードの修正をお願いする",
+          },
+          Optimize = {
+            prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
+            mapping = '<leader>co',
+            description = "コードの最適化をお願いする",
+          },
+          Docs = {
+            prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
+            mapping = '<leader>cd',
+            description = "コードのドキュメント作成をお願いする",
+          },
+          Tests = {
+            prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
+            mapping = '<leader>ct',
+            description = "テストコード作成をお願いする",
+          },
+          FixDiagnostic = {
+            prompt = 'コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。',
+            mapping = '<leader>cd',
+            description = "コードの修正をお願いする",
+            selection = require('CopilotChat.select').diagnostics,
+          },
+          Commit = {
+            prompt =
+            '実装差分に対するコミットメッセージを日本語で記述してください。',
+            mapping = '<leader>cco',
+            description = "コミットメッセージの作成をお願いする",
+            selection = require('CopilotChat.select').gitdiff,
+          },
+          CommitStaged = {
+            prompt =
+            'ステージ済みの変更に対するコミットメッセージを日本語で記述してください。',
+            mapping = '<leader>cs',
+            description = "ステージ済みのコミットメッセージの作成をお願いする",
+            selection = function(source)
+              return require('CopilotChat.select').gitdiff(source, true)
+            end,
+          },
+        },
+      })
+    end
+    -- See Commands section for default commands if you want to lazy load on them
   },
 
   -- NOTE: This is where your plugins related to LSP can be installed.
@@ -823,8 +895,11 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
+    -- { name = 'copilot' },
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'buffer', keyword_length = 2 },
+    { name = 'path' },
   },
 }
 
